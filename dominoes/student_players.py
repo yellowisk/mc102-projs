35e05af75f67310d8b3e4d2a121e38ed49788e38
yellowisk <oheitoralmeida2013@gmail.com>
2024-06-19 17:08:39 -0300

    feat: add new images and format code

--- dominoes/student_players.py
+++ dominoes/student_players.py
@@ -7,11 +7,10 @@ from basic_players import Player
 
 class Weights():
     """
-    Decidimos em nosso projeto adicionar um valor numérico a cada peça de \n
-    dominó em nosa mão para depois -na função general e sabotage- para isso,\n
-    criamos uma classe Weights em que cada jogador terá seu próprio objeto\n
-    dessa classe e dentro da classe também estão as funções utilizadas para\n
-    aumentar tal valor numérico.
+    Decidimos em nosso projeto adicionar um valor numérico a cada peça de dominó em nosa mão para depois \n
+    -na função general e sabotage- para isso, criamos uma classe Weights em que cada jogador terá seu \n
+    próprio objeto dessa classe e dentro da classe também estão as funções utilizadas para aumentar tal \n
+    valor numérico.
     """
     def __init__(self, hand) -> None:
         self._store = self.gen(hand)
@@ -19,7 +18,6 @@ class Weights():
     @property
     def store(self):
         return self._store
-
     @store.setter
     def store(self, store):
         self._store = store
@@ -29,6 +27,7 @@ class Weights():
         for h in hand:
             store[(h[0], h[1], h[0]+h[1])] = 0
         return store
+
 
     def propria_mao(self, tiles):
         """
@@ -50,36 +49,35 @@ class Weights():
 
         for i in tiles:
             if i[0] in mao.keys():
-                self.store[(i[0],i[1],i[0]+i[1])]+=mao[i[0]]
+                self.store[(i[0], i[1], i[0]+i[1])] += mao[i[0]]
             if i[1] in mao.keys():
-                self.store[(i[0],i[1],i[0]+i[1])]+=mao[i[1]]
+                self.store[(i[0], i[1], i[0]+i[1])] += mao[i[1]]
 
-    def ajuda_dupla(self,jogadas,tiles):
+    def ajuda_dupla(self, jogadas, tiles):
         """
         Adicionamos ao dicionário da classe Weights 3 pontos a cada vez que um dominó que temos em nossa \n
         mão tem um número que foi jogado na última jogada de seu aliado, o que depois servirá para \n
         decidir qual peça que será jogada.
         """
-        if len(jogadas)>=3:
-            p_ally=jogadas[-2]
+        if len(jogadas) >= 3:
+            p_ally = jogadas[-2]
             for i in tiles:
                 if i[0] in p_ally[1] or i[1] in p_ally[1]:
-                    self.store[(i[0],i[1],i[0]+i[1])]+=15
+                    self.store[(i[0], i[1], i[0]+i[1])] += 15
 
-    def atrapalha_inimo(self,jogadas,tiles):
+
+    def atrapalha_inimo(self, jogadas, tiles):
         """
         Adicionamos ao dicionário da classe Weights 3 pontos a cada vez que um dominó que temos em nossa \n
         mão não tem um número que foi jogado na última jogada de seu oponente não anterior quando ele não \n
         conseguir jogar uma peça, o que depois servirá para decidir qual peça será jogada.
         """
-        if len(jogadas)>=1:
-            p_inimigo=jogadas[-1]
-            #if p_inimigo[3]==None:
+        if len(jogadas) >= 1:
+            p_inimigo = jogadas[-1]
             for i in tiles:
                 if i[0] not in p_inimigo[1] or i[1] not in p_inimigo[1]:
-                    self.store[(i[0],i[1],i[0]+i[1])]+=15
-
-    def sabotage(self,jogadas,ferradas, extremos, mao):
+                    self.store[(i[0], i[1], i[0]+i[1])] += 15
+    def sabotage(self, jogadas, ferradas, extremos, mao):
         """
         Função que decide qual peça será jogada para atrapalhar o inimigo. Verifica-se primeiro se \n
         a antepenúltima jogada, a do inimigo, foi None, ou seja, o inimigo considerou essa peça \n
@@ -88,9 +86,9 @@ class Weights():
         alguma peça que possa ser jogada em cima dessa peça. Caso sim, adicionamos as possibilidades \n
         a uma lista e escolhemos a peça com maior peso para ser jogada.
         """
-        if len(jogadas)>=3:
-            if jogadas[-3][3] is None:
-                extremo=jogadas[-3][1]
+        if len(jogadas) >= 3:
+            if jogadas[-3][3] == None:
+                extremo = jogadas[-3][1]
                 ferradas.append(extremo[0])
                 ferradas.append(extremo[1])
                 possibilidade = []
@@ -113,65 +111,65 @@ class Weights():
                 if len(possibilidade) == 0:
                     return None
                 maior_peso =- 1
-                melhor=possibilidade[0]
+                melhor = possibilidade[0]
                 for i in possibilidade:
-                    if self.store[i[0],i[1],i[0]+i[1]]>maior_peso:
-                        melhor= possibilidade[possibilidade.index(i)]
-                return melhor[2],(melhor[0],melhor[1])
+                    if self.store[i[0], i[1], i[0]+i[1]]>maior_peso:
+                        melhor = possibilidade[possibilidade.index(i)]
+                return melhor[2], (melhor[0], melhor[1])
             else:
                 return None
         else:
             return None
 
 
-    def general(self, tiles,extremos):
+    def general(self, tiles, extremos):
         """
         Função que decide qual peça será jogada. Primeiramente escokhe-se a peça com maior peso e caso haja mais de uma, \n
         escolhe-se a peça que tem a maior soma de seus lados entre as duas peças com maior peso. Caso não haja peças que \n 
         possam ser jogadas, retorna-se None.
         """
-        playable=[]
-        pesos=[]
-        maior_soma=None
-        if len(extremos)==0:
+        playable = []
+        pesos = []
+        maior_soma = None
+        if len(extremos) == 0:
             for i in tiles:
                     playable.append(i)
                     pesos.append(self._store[i])
 
-            melhor=max(pesos)
-            melhor_melhor=-1
+            maior = max(pesos)
+            best =- 1
 
-            if pesos.count(melhor)>1:
-                for i in range(pesos.count(melhor)):
-                    if playable[pesos.index(melhor)][2]>melhor_melhor:
-                        melhor_melhor=playable[pesos.index(melhor)][2]
-                        maior_soma=(playable[pesos.index(melhor)][0],playable[pesos.index(melhor)][1])
-                    pesos[pesos.index(melhor)]-=1
+            if pesos.count(maior) > 1:
+                for i in range(pesos.count(maior)):
+                    if playable[pesos.index(maior)][2] > best:
+                        best = playable[pesos.index(maior)][2]
+                        maior_soma = (playable[pesos.index(maior)][0], playable[pesos.index(maior)][1])
+                    pesos[pesos.index(maior)] -= 1
                 return maior_soma
             else:
-                return (playable[pesos.index(melhor)][0],playable[pesos.index(melhor)][1])
+                return (playable[pesos.index(maior)][0], playable[pesos.index(maior)][1])
             
-        if len(tiles)!=0:
+        if len(tiles) != 0:
             for i in tiles:
                 if i[0] in extremos or i[1] in extremos:
                     playable.append(i)
                     pesos.append(self._store[i])
 
-            if len(playable)==0:
+            if len(playable) == 0:
                 return maior_soma
             
-            melhor=max(pesos)
-            melhor_melhor=-1
+            maior = max(pesos)
+            best =- 1
 
-            if pesos.count(melhor)>1:
-                for i in range(pesos.count(melhor)):
-                    if playable[pesos.index(melhor)][2]>melhor_melhor:
-                        melhor_melhor=playable[pesos.index(melhor)][2]
-                        maior_soma=(playable[pesos.index(melhor)][0],playable[pesos.index(melhor)][1])
-                    pesos[pesos.index(melhor)]-=1
+            if pesos.count(maior)>1:
+                for i in range(pesos.count(maior)):
+                    if playable[pesos.index(maior)][2] > best:
+                        best = playable[pesos.index(maior)][2]
+                        maior_soma=(playable[pesos.index(maior)][0], playable[pesos.index(maior)][1])
+                    pesos[pesos.index(maior)] -= 1
                 return maior_soma
             else:
-                return (playable[pesos.index(melhor)][0],playable[pesos.index(melhor)][1])
+                return (playable[pesos.index(maior)][0], playable[pesos.index(maior)][1])
             
         return maior_soma
 
@@ -179,12 +177,12 @@ class Weights():
 class Jogador2(Player):
 
     def __init__(self):
-        super().__init__(0, "2")
-        pesosdois=Weights(self.tiles)
+        super().__init__(281801, "Dor", "img/dor.gif")
+        pesosdois = Weights(self.tiles)
         self._weights = pesosdois
         self._doublesdois = None
         self._not_doublesdois = None
-        self._ferradasdois= []
+        self._ferradasdois = []
 
     def split_tiles(self):
         """
@@ -194,34 +192,31 @@ class Jogador2(Player):
         not_doubles = []
         for tile in self.tiles:
             if tile[0] == tile[1]:
-                doubles.append((tile[0],tile[1],tile[0]+tile[1]))
+                doubles.append((tile[0], tile[1], tile[0]+tile[1]))
             else:
-                not_doubles.append((tile[0],tile[1],tile[0]+tile[1]))
-        self._doublesdois=doubles
+                not_doubles.append((tile[0], tile[1], tile[0]+tile[1]))
+        self._doublesdois = doubles
         self._not_doublesdois = not_doubles
 
     def play(self, board_extremes, play_hist):
         self.split_tiles()
-        #self._weights.ajuda_dupla(play_hist,self._tiles,board_extremes)
-        #self._weights.atrapalha_inimo(play_hist,self._tiles)
-        if len(self.tiles)==10:
-            self._weights.store=self._weights.gen(self.tiles)
+
+        self._weights.store = self._weights.gen(self.tiles)
         self._weights.propria_mao(self.tiles)
-        self._weights.ajuda_dupla(play_hist,self._tiles)
-        self._weights.atrapalha_inimo(play_hist,self._tiles)
+        self._weights.ajuda_dupla(play_hist, self._tiles)
 
-        if len(board_extremes)==0:
-            if len(self._doublesdois)!=0:
-                return 1, self._weights.general(self._doublesdois,board_extremes)
-            return 1, self._weights.general(self._not_doublesdois,board_extremes)
+        if len(board_extremes) == 0:
+            if len(self._doublesdois) != 0:
+                return 1, self._weights.general(self._doublesdois, board_extremes)
+            return 1, self._weights.general(self._not_doublesdois, board_extremes)
 
-        if self._weights.sabotage(play_hist,self._ferradasdois,board_extremes,self.tiles)!=None:
-            return self._weights.sabotage(play_hist,self._ferradasdois,board_extremes,self.tiles)
+        if self._weights.sabotage(play_hist, self._ferradasdois, board_extremes, self.tiles) != None:
+            return self._weights.sabotage(play_hist, self._ferradasdois, board_extremes, self.tiles)
         
-        if self._weights.general(self._doublesdois,board_extremes)!=None:
-            return 1, self._weights.general(self._doublesdois,board_extremes)
+        if self._weights.general(self._doublesdois, board_extremes) != None:
+            return 1, self._weights.general(self._doublesdois, board_extremes)
         
-        return 1, self._weights.general(self._not_doublesdois,board_extremes)
+        return 1, self._weights.general(self._not_doublesdois, board_extremes)
 
     
 
@@ -229,12 +224,12 @@ class Jogador2(Player):
 class Jogador1(Player):
 
     def __init__(self):
-        super().__init__(0, "1")
-        pesos=Weights(self.tiles)
+        super().__init__(245293, "Tristeza", "img/tristeza.gif")
+        pesos = Weights(self.tiles)
         self._weights = pesos
         self._doubles = None
         self._not_doubles = None
-        self._ferradas= []
+        self._ferradas = []
 
     def split_tiles(self):
         """
@@ -244,40 +239,38 @@ class Jogador1(Player):
         not_doubles = []
         for tile in self.tiles:
             if tile[0] == tile[1]:
-                doubles.append((tile[0],tile[1],tile[0]+tile[1]))
+                doubles.append((tile[0], tile[1], tile[0]+tile[1]))
             else:
-                not_doubles.append((tile[0],tile[1],tile[0]+tile[1]))
-        self._doubles=doubles
+                not_doubles.append((tile[0], tile[1], tile[0]+tile[1]))
+        self._doubles = doubles
         self._not_doubles = not_doubles
 
     def play(self, board_extremes, play_hist):
         self.split_tiles()
 
-        if len(self.tiles)==10:
-            self._weights.store=self._weights.gen(self.tiles)
+        self._weights.store = self._weights.gen(self.tiles)
 
         self._weights.propria_mao(self.tiles)
-        self._weights.ajuda_dupla(play_hist,self._tiles)
-        self._weights.atrapalha_inimo(play_hist,self._tiles)
-        #self._weights.atrapalha_inimo(play_hist,self._tiles)
+        self._weights.ajuda_dupla(play_hist, self._tiles)
         
-        if len(board_extremes)==0:
-            if len(self._doubles)!=0:
-                return 1, self._weights.general(self._doubles,board_extremes)
-            return 1, self._weights.general(self._not_doubles,board_extremes)
+        
+        if len(board_extremes) == 0:
+            if len(self._doubles) != 0:
+                return 1, self._weights.general(self._doubles, board_extremes)
+            return 1, self._weights.general(self._not_doubles, board_extremes)
 
-        if self._weights.sabotage(play_hist,self._ferradas,board_extremes,self.tiles)!=None:
-            return self._weights.sabotage(play_hist,self._ferradas,board_extremes,self.tiles)
+        if self._weights.sabotage(play_hist, self._ferradas, board_extremes, self.tiles) != None:
+            return self._weights.sabotage(play_hist, self._ferradas, board_extremes, self.tiles)
         
-        if self._weights.general(self._doubles,board_extremes)!=None:
-            return 1, self._weights.general(self._doubles,board_extremes)
+        if self._weights.general(self._doubles, board_extremes) != None:
+            return 1, self._weights.general(self._doubles, board_extremes)
         
-        return 1, self._weights.general(self._not_doubles,board_extremes)
+        return 1, self._weights.general(self._not_doubles, board_extremes)
 
 
 # Função que define o nome da dupla:
 def pair_name():
-    return "Placeholder" # Defina aqui o nome da sua dupla
+    return "Aflição" # Defina aqui o nome da sua dupla
 
 # Função que cria a dupla:
 def create_pair():
